bf5670991d1d08eebe3cac7b8539e2a834a1938c
evgalp <evgeniyalpayev@gmail.com>
2017-06-30 11:32:09 +0300

    styled

--- app/components/AddTodo.jsx
+++ app/components/AddTodo.jsx
@@ -16,7 +16,7 @@ var AddTodo = React.createClass({
 	},
 	render: function () {
 		return (
-			<div>
+			<div className="container__footer">
 				<form ref="form" onSubmit={this.onSubmit}>
 					<input ref="todoText" type="text" placeholder="Type in to do item"/>
 					<button className="button expanded">Add item</button>
