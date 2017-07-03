591414f8131b00ac9abba1900e63cea8c999c194
evgalp <evgeniyalpayev@gmail.com>
2017-07-03 17:10:08 +0300

    reducers searchText and showCopleted

---
+++ app/reducers/reducers.jsx
@@ -0,0 +1,17 @@
+export var searchTextReducer = function (state = '', action) {
+	switch (action.type) {
+		case 'SET_SEARCH_TEXT':
+			return action.searchText;
+		default:
+			return state;
+	}
+};
+
+export var showCompletedReducer = function (state = false, action) {
+	switch (action.type) {
+		case 'TOGGLE_SHOW_COMPLETED':
+			return !state;
+		default:
+			return state;
+	}
+};
