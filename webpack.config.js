25d4dcf37641c788721b5671a0d311654d8b2f19
evgalp <evgeniyalpayev@gmail.com>
2017-08-15 14:29:54 +0300

    asd

--- webpack.config.js
+++ webpack.config.js
@@ -5,23 +5,23 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 module.exports = {
   entry: [
-    'script!jquery/dist/jquery.min.js',
-    'script!foundation-sites/dist/foundation.min.js',
-    './app/app.jsx'
+  'script!jquery/dist/jquery.min.js',
+  'script!foundation-sites/dist/foundation.min.js',
+  './app/app.jsx'
   ],
   externals: {
     jquery: 'jQuery'
   },
   plugins: [
-    new webpack.ProvidePlugin({
-      '$': 'jquery',
-      'jQuery': 'jquery'
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      compressor: {
-        warnings: false
-      }
-    })
+  new webpack.ProvidePlugin({
+    '$': 'jquery',
+    'jQuery': 'jquery'
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compressor: {
+      warnings: false
+    }
+  })
   ],
   output: {
     path: __dirname,
@@ -30,9 +30,9 @@ module.exports = {
   resolve: {
     root: __dirname,
     modulesDirectories: [
-      'node_modules',
-      './app/components',
-      './app/api'
+    'node_modules',
+    './app/components',
+    './app/api'
     ],
     alias: {
       app: 'app',
@@ -46,19 +46,19 @@ module.exports = {
   },
   module: {
     loaders: [
-      {
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'es2015', 'stage-0']
-        },
-        test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/
-      }
+    {
+      loader: 'babel-loader',
+      query: {
+        presets: ['react', 'es2015', 'stage-0']
+      },
+      test: /\.jsx?$/,
+      exclude: /(node_modules|bower_components)/
+    }
     ]
   },
   sassLoader: {
     includePaths: [
-      path.resolve(__dirname, './node_modules/foundation-sites/scss')
+    path.resolve(__dirname, './node_modules/foundation-sites/scss')
     ]
   },
   devtool: process.env.NODE_ENV === 'production' ? undefined : 'cheap-module-eval-source-map'
